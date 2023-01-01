1667d1971c939b98bf2145b426f298140d66baac
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2023-01-01 22:56:47 +0000

    ics3a/scripts: More cmd line options for test_irrv
    - Option to specify different content resolution than the game
      (Required to enable tests that change resolution)
    - Expose other controls on script cmd line (quality, bitrate settings)
    
    Related Jira: VSMGWL-51998

--- scripts/test_irrv.sh
+++ scripts/test_irrv.sh
@@ -1,24 +1,102 @@
 #!/bin/bash
 
-# Usage:
-#  test_irrv.sh /path/to/aic/workdir streaming:latest icr:latest Profile.yml WidthxHeight [test-set]
-#
-# -- Profile.yml is expected to be in /path/to/aic/workdir
-# -- test-set is optional. Supported options: "all" (default), "basic".
-#    - Any input other than supported ones above will use the default option
+function Usage()
+{
+
+    cat << EOF
+test_irrv.sh /path/to/aic/workdir streaming:latest icr:latest Profile.yml GameResolution
+              [-t test-set] [-s SrcWidthxSrcHeight] [-q quality] [-b target-bitrate] [-mb max-bitrate]
+
+The first five arguments are mandatory:
+-- Profile.yml is expected to be in /path/to/aic/workdir
+-- GameResolution is specified as WidthxHeight (e.g. 1920x1080). This is the resolution that the game
+   and encoder (with alignment adjustments) are expected to initialize with.
+
+Optional Args:
+* -t  | --test-set : Test set to use. Any input outside supported set will use default.
+                     Supported options: "all" (default), "basic".
+  -s  | --src-res  : Specify resolution of test source content in WxH format (e.g.1280x720).
+                     This should be >= Game resolution. Default value = Game Resolution. 
+                     If test allows resolution change, this should be >= Maximum of all resolutions in test
+* -q  | --quality  : TU Setting for Encoder. Any input outside supported set will use default
+                     Supported options: 1,4,7. Default Value = 4 (TU4)
+* -b  | --target-bitrate : Target bitrate to use in test (e.g. 3.2M). Default value = 3.3M
+* -mb |--max-bitrate     : Max bitrate to use in test (e.g. 5.4M). Default value = 6.6M
+EOF
+}
+
+function parse_optional_args()
+{
+    while (("$#"))
+    do
+        case "$1" in
+            -t | --test-set )
+                TEST_SET=$2
+                shift 2
+                ;;
+            -s | --src-res )
+                SRC_RESOLUTION=$2
+                shift 2
+                ;;
+            -q | --quality )
+                QUALITY=$2
+                [$2 -ne 1] && [$2 -ne 4] && [$2 -ne 7] && QUALITY=4
+                shift 2
+                ;;
+            -b | --target-bitrate )
+                TARGET_BITRATE=$2
+                shift 2
+                ;;
+            -mb | --max-bitrate )
+                MAX_BITRATE=$2
+                shift 2
+                ;;
+            *)
+                echo "Unknown argument: $1"
+                shift
+                ;;
+        esac
+    done
+}
+
+function get_framerate() {
+  echo "$1" | sed -E 's/.*fr ([0-9\.]*).*/\1/'
+}
+
+function is_av1_test() {
+  if echo $1 | grep -q av1; then
+    return 0
+  fi
+  return 1
+}
+
+function check_av1() {
+  if $VAINFO_DOCKER vainfo --display drm --device $DEVICE 2>/dev/null | grep VAEntrypointEncSliceLP | grep -q VAProfileAV1; then
+    return 0
+  fi
+  return 1
+}
 
 SCRIPT_PATH=$(dirname $(which $0))
 
+[ $# -lt 5 ] && Usage && exit
+
+#Mandatory Args
 AIC_WORKDIR=$(realpath $1)
 ESC=$2
 ICR=$3
 PROFILE=$4
-RESOLUTION=$5
-TEST_SET=${6:-all}
+GAME_RESOLUTION=$5
 
+#Optional args
+TEST_SET=all
+SRC_RESOLUTION=$GAME_RESOLUTION
 QUALITY=4
 TARGET_BITRATE=3.3M
 MAX_BITRATE=6.6M
+
+shift 5
+parse_optional_args $@
 
 set -x
 
@@ -62,53 +140,36 @@ VAINFO_DOCKER="docker run --rm \
   --name vainfo0 \
   $ICR"
 
-ffmpeg_cmd="ffmpeg -f lavfi -i testsrc=size=${RESOLUTION}:rate=30 -pix_fmt bgra -frames:v 150 -f rawvideo -y /opt/workdir/test.bgra"
+ffmpeg_cmd="ffmpeg -f lavfi -i testsrc=size=${SRC_RESOLUTION}:rate=30 -pix_fmt bgra -frames:v 150 -f rawvideo -y /opt/workdir/test.bgra"
 
 icr_cases_all=(
   # h264
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 30 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 30 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 60 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 60 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
   # h265
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 30 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 30 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 60 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 60 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
   # av1
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 60 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 60 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
 )
 
 icr_cases_basic=(
   # Basic set covering Codec, FrameRate and TCAE settings
   # h264
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 30 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
   # h265
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 60 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
   # av1
-  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${GAME_RESOLUTION} -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
 )
 
-function get_framerate() {
-  echo "$1" | sed -E 's/.*fr ([0-9\.]*).*/\1/'
-}
-
-function is_av1_test() {
-  if echo $1 | grep -q av1; then
-    return 0
-  fi
-  return 1
-}
-
-function check_av1() {
-  if $VAINFO_DOCKER vainfo --display drm --device $DEVICE 2>/dev/null | grep VAEntrypointEncSliceLP | grep -q VAProfileAV1; then
-    return 0
-  fi
-  return 1
-}
 
 $FFMPEG_DOCKER $ffmpeg_cmd
 
