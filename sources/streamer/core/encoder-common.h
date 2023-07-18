ce806ac81ad5365a47b26b7aaae579679cc21286
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2023-07-18 15:02:15 -0700

    streamer: prune unused code
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- sources/streamer/core/encoder-common.h
+++ sources/streamer/core/encoder-common.h
@@ -30,34 +30,6 @@
 #include "ga-module.h"
 #include "cursor.h"
 
-/*
- * Packet format for encoder packet queue.
- *
- * This data structure should be read-only outside encoder-common.cpp
- */
-typedef struct encoder_packet_s {
-    char *data;        /**< Pointer to the data buffer */
-    unsigned size;        /**< Size of the buffer */
-    int64_t pts_int64;    /**< Packet timestamp in a 64-bit integer */
-    struct timeval pts_tv;    /**< Packet timestamp in \a timeval structure */
-    // internal data structure - do not touch
-    int padding;        /**< Padding area: internal used */
-}    encoder_packet_t;
-
-typedef struct encoder_packet_queue_s {
-    std::mutex mutex;    /**< Per-queue mutex */
-    char *buf;        /**< Pointer to the packet queue buffer */
-    int bufsize;        /**< Size of the queue buffer */
-    int datasize;        /**< Size of occupied data size */
-    int head;        /**< Position of queue head */
-    int tail;        /**< Position of queue tail */
-}    encoder_packet_queue_t;
-
-typedef struct encoder_pts_s {
-    long long pts;
-    struct timeval ptv;
-}    encoder_pts_t;
-
 typedef struct _FrameMetaData {
     bool last_slice;
     uint64_t capture_time_ms;
@@ -71,7 +43,6 @@ typedef struct _FrameMetaData {
 
 typedef void (*qcallback_t)(int);
 
-EXPORT int encoder_pts_sync(int samplerate);
 EXPORT int encoder_running();
 EXPORT int encoder_register_vencoder(ga_module_t *m, void *param);
 EXPORT int encoder_register_aencoder(ga_module_t *m, void *param);
@@ -89,21 +60,6 @@ EXPORT int encoder_send_qos(std::shared_ptr<QosInfo> qosInfo);
 EXPORT int SendBitstream(const unsigned char*bitStream, unsigned int sizeBits);
 EXPORT int GetCreditBytes();
 #endif
-// encoder pts to ptv mapping function
-EXPORT int encoder_pts_clear(unsigned queueid);
-EXPORT int encoder_pts_put(unsigned queueid, long long pts, struct timeval *ptv);
-EXPORT struct timeval * encoder_ptv_get(unsigned queueid, long long pts, struct timeval *ptv, int interpolation);
-
-// encoder packet queue - for async packet delivery
-EXPORT int encoder_pktqueue_init(int channels, int qsize);
-EXPORT int encoder_pktqueue_reset();
-EXPORT int encoder_pktqueue_reset_channel(int channelId);
-EXPORT int encoder_pktqueue_size(int channelId);
-EXPORT int encoder_pktqueue_append(int channelId, ga_packet_t *pkt, int64_t encoderPts, struct timeval *ptv);
-EXPORT char * encoder_pktqueue_front(int channelId, encoder_packet_t *pkt);
-EXPORT void encoder_pktqueue_split_packet(int channelId, char *offset);
-EXPORT void encoder_pktqueue_pop_front(int channelId);
-EXPORT int encoder_pktqueue_register_callback(int channelId, qcallback_t cb);
-EXPORT int encoder_pktqueue_unregister_callback(int channelId, qcallback_t cb);
 
 #endif
+
